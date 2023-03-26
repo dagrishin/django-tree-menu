dd69f39b473ac7988eb4ca8278888eb72043586f
Dmitriy <grishin-dim@yandex.ru>
2023-03-26 13:01:00 +0300

    register model menu in admin panel

--- menuapp/admin.py
+++ menuapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from menuapp.models import MenuItem
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'menu_name', 'url', 'order']
+    list_filter = ['menu_name']
+    search_fields = ['title', 'url']
+    ordering = ['order', 'title']
